a6ad7481d46d708261ad79db0ba0da4f9619bdfe
Vojtěch Vít <i.am@vojtechvit.cz>
2015-05-03 17:48:36 +0200

    Added an empty OWIN-hosted website.

---
+++ src/ODataFileRepository.Website/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(ODataFileRepository.Website.Startup))]
+
+namespace ODataFileRepository.Website
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
