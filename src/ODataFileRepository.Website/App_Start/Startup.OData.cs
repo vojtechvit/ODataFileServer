c22747bc340c14844dae84168a0ef005b54c3fc8
Vojtěch Vít <vojtech.vit@frontiersin.org>
2015-05-20 17:36:25 +0200

    improved model definition

--- src/ODataFileRepository.Website/App_Start/Startup.OData.cs
+++ src/ODataFileRepository.Website/App_Start/Startup.OData.cs
@@ -4,6 +4,7 @@ using ODataFileRepository.Website.ServiceModels;
 using System;
 using System.Globalization;
 using System.Reflection;
+using System.Text;
 using System.Web.OData.Builder;
 
 namespace ODataFileRepository.Website
@@ -14,8 +15,6 @@ namespace ODataFileRepository.Website
         {
             private const string TypeNamespace = "type";
 
-            private const string FileTypeName = "file";
-
             public static IEdmModel CreateModel()
             {
                 var modelBuilder = new ODataConventionModelBuilder();
@@ -24,19 +23,42 @@ namespace ODataFileRepository.Website
                 modelBuilder.ContainerName = "fileRepository";
 
                 var fileType = modelBuilder.EntityType<File>();
-                fileType.Name = FileTypeName;
+                fileType.Name = Camelize(fileType.Name);
                 fileType.Namespace = TypeNamespace;
 
                 modelBuilder.EntitySet<File>("files");
 
                 var model = modelBuilder.GetEdmModel() as EdmModel;
 
-                var edmFileType = model.FindDeclaredType(string.Join(".", TypeNamespace, FileTypeName)) as EdmEntityType;
+                var edmFileType = model.FindDeclaredType(QualifiedTypeName(fileType.Name)) as EdmEntityType;
                 SetPrivateFieldValue(edmFileType, "hasStream", true);
 
                 model.SetDescriptionAnnotation(edmFileType, "Represents a file in the file repository.");
 
                 return model;
+            }
+
+            private static string QualifiedTypeName(string localTypeName)
+            {
+                return string.Join(".", TypeNamespace, localTypeName);
+            }
+
+            private static string Camelize(string text)
+            {
+                if (text == null)
+                {
+                    throw new ArgumentNullException("text");
+                }
+
+                if (text.Length == 0)
+                {
+                    return text;
+                }
+
+                var stringBuilder = new StringBuilder(text);
+                stringBuilder[0] = char.ToLowerInvariant(text[0]);
+
+                return stringBuilder.ToString();
             }
 
             private static void SetPrivateFieldValue<T>(object obj, string propName, T val)
