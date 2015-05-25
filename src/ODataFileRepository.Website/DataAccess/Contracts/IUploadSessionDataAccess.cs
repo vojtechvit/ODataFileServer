5f3f3e9167765894c1bbc18ce9464849d82ee09c
Vojtěch Vít <i.am@vojtechvit.cz>
2015-05-26 00:11:47 +0200

    Added forgotten files of commit 25a0ff3784a8fe90acb76b30c7b8b18848819462

---
+++ src/ODataFileRepository.Website/DataAccess/Contracts/IUploadSessionDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using ODataFileRepository.Website.DataAccessModels.Contracts;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ODataFileRepository.Website.DataAccess.Contracts
+{
+    public interface IUploadSessionDataAccess
+    {
+        Task<IUploadSession> CreateAsync(
+            string uploadSessionIdentifier,
+            string fileIdentifier, 
+            string fileName);
+
+        Task<bool> ExistsAsync(string uploadSessionIdentifier);
+
+        [SuppressMessage("Microsoft.Design", "CA1024:UsePropertiesWhereAppropriate",
+            Justification = "Represents an operation, not a property of this class.")]
+        Task<IReadOnlyList<IUploadSession>> GetAllAsync();
+
+        Task<IUploadSession> GetAsync(string uploadSessionIdentifier);
+
+        Task<IUploadSession> UploadSegmentAsync(
+            string uploadSessionIdentifier, 
+            string mediaType, 
+            long rangeFrom,
+            long rangeTo,
+            long rangeLength, 
+            Stream stream);
+
+        Task<IUploadSession> DeleteAsync(string uploadSessionIdentifier);
+    }
+}
